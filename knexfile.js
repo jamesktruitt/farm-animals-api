6244820863948f4f95f32d4b86e73708f2e6a095
James Truitt <tru@rockyagirl.com>
2018-04-02 08:13:31 -0600

    created Heroku server

--- knexfile.js
+++ knexfile.js
@@ -3,6 +3,10 @@ require("dotenv").load();
 module.exports = {
 	development: {
 		client: "pg",
-		connection: `postgres:///${process.env.LOCAL_DATABASE_NAME}` 
+		connection: `postgres:///${process.env.LOCAL_DATABASE_NAME}`
 	},
+	production: {
+		client: "pg",
+		connection: `${process.env.DATABASE_URL}?ssl=true`
+	}
 };
